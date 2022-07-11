5559f1c9f3fd8f3015856d8cd18b54f92057257a
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-07-11 15:43:09 +0430

    get post by primary key added

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list_view, name='posts_list')
+    path('', views.post_list_view, name='posts_list'),
+    path('<int:pk>/', views.post_detail_view, name='post_detail'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ def post_list_view(request):
     posts_list = Post.objects.all()
     return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
 
+
+def post_detail_view(request, pk):
+    sample_post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': sample_post})
+
