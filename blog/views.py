d3934d350f13da4f6a7987fbf99257af6161c6e4
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-07-11 17:16:27 +0430

    primary key error (404) handled

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Post
+
 
 def post_list_view(request):
     posts_list = Post.objects.all()
@@ -7,6 +9,10 @@ def post_list_view(request):
 
 
 def post_detail_view(request, pk):
-    sample_post = Post.objects.get(pk=pk)
+    sample_post = get_object_or_404(Post, pk=pk)
+    # try:
+    #     sample_post = Post.objects.get(pk=pk)
+    # except ObjectDoesNotExist:
+    #     sample_post = None
+    #     print('Excepted')
     return render(request, 'blog/post_detail.html', {'post': sample_post})
-
