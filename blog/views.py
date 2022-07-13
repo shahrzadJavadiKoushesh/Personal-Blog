b905996413d2e299aa31ec269d74290cb804ab8b
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-07-14 02:02:53 +0430

    creat new post page added & blog link modified

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,7 @@ def post_list_view(request):
 def post_detail_view(request, pk):
     sample_post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': sample_post})
+
+
+def post_create_view(request):
+    return render(request, 'blog/post_create.html')
