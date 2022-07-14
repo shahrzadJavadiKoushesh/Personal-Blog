6af7f8567d721885f18fbefd8937d749cef61c4f
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-07-15 02:23:40 +0430

    Revert "creat new post page added & blog link modified"
    This reverts commit b9059964

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list_view, name='posts_list'),
     path('<int:pk>/', views.post_detail_view, name='post_detail'),
-    path('create/', views.post_create_view, name='post_create')
+
 ]
