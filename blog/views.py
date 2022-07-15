3390068742824353c568c02b4798c5da611cb4ca
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-07-15 22:33:30 +0430

    using model forms

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Post
 from django.contrib.auth.models import User
+from .forms import NewPostForm
 
 
 def post_list_view(request):
@@ -16,11 +17,17 @@ def post_detail_view(request, pk):
 
 def post_create_view(request):
     if request.method == 'POST':
-        post_title = request.POST.get('title')
-        post_text = request.POST.get('text')
-        # django ORM
-        sample_user = User.objects.all()[0]
-        Post.objects.create(title=post_title, text=post_text, author=sample_user, status='pub')
-    else:
-        print("GET request")
-    return render(request, 'blog/post_create.html')
+        pass
+    else: # GET request
+        form = NewPostForm()
+    return render(request, 'blog/post_create.html', context={'form': form})
+
+    # if request.method == 'POST':
+    #     post_title = request.POST.get('title')
+    #     post_text = request.POST.get('text')
+    #     # django ORM
+    #     sample_user = User.objects.all()[0]
+    #     Post.objects.create(title=post_title, text=post_text, author=sample_user, status='pub')
+    # else:
+    #     print("GET request")
+    # return render(request, 'blog/post_create.html')
