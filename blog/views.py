d4f17206af48fddc97f2f9334542e654a0cb5686
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-07-15 21:46:09 +0430

    save post data in database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Post
+from django.contrib.auth.models import User
 
 
 def post_list_view(request):
@@ -14,4 +15,12 @@ def post_detail_view(request, pk):
 
 
 def post_create_view(request):
+    if request.method == 'POST':
+        post_title = request.POST.get('title')
+        post_text = request.POST.get('text')
+        # django ORM
+        sample_user = User.objects.all()[0]
+        Post.objects.create(title=post_title, text=post_text, author=sample_user, status='pub')
+    else:
+        print("GET request")
     return render(request, 'blog/post_create.html')
