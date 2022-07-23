87044f61788a20266684d79089c76ea2460210d3
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-07-23 13:23:44 +0430

    class-based view for post list view

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render, redirect, reverse
 from django.shortcuts import get_object_or_404
-from .models import Post
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
 from .forms import NewPostForm
+from .models import Post
+from django.views import generic
 
 
-def post_list_view(request):
-    posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
-    return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
+# def post_list_view(request):
+#     posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
+#     return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
+
+# class based view for post_list_view method
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'blog/posts_list.html'
+    context_object_name = 'posts_list'
+
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('datetime_modified')
 
 
 def post_detail_view(request, pk):
